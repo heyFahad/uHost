bb3de8d89d33f118248eb8ed7855bb06f531ebd5
Fahad Javed <Fahad_Javed008@yahoo.com>
2019-09-19 16:07:14 +0500

    Opening and closing the hamburger menu using Javascript and finally cleaning up the code.

--- public/shared.js
+++ public/shared.js
@@ -2,18 +2,40 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        backdrop.classList.add('open');
+        modal.classList.add('open');
     });
 }
 
-backdrop.addEventListener('click', closeModal);
-modalNoButton.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function () {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    backdrop.classList.remove('open');
+    if (modal) {
+        modal.classList.remove('open');
+    }
 }
+
+toggleButton.addEventListener('click', function () {
+    // backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+    backdrop.classList.add('open');
+    mobileNav.classList.add('open');
+});
